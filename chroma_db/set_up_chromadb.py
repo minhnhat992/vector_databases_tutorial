aed9f9ec0739f62aa857c40c155482ed1a22267b
minhbui <minh.bui@wirecutter.com>
2024-01-10 16:20:24 -0500

    add chroma

---
+++ chroma_db/set_up_chromadb.py
@@ -0,0 +1,59 @@
+"""Set up a vector database, so you can make query search on documents."""
+
+import chromadb
+from chromadb.utils import embedding_functions
+
+CHROMA_DATA_PATH = "chroma_data/"
+EMBED_MODEL = "all-MiniLM-L6-v2"
+COLLECTION_NAME = "demo_docs"
+
+# set up client and collections
+client = chromadb.PersistentClient(path=CHROMA_DATA_PATH)
+
+embedding_funcs = embedding_functions.SentenceTransformerEmbeddingFunction(
+    model_name=EMBED_MODEL
+)
+
+collection = client.create_collection(
+    name=COLLECTION_NAME,
+    embedding_function=embedding_funcs,
+    metadata={"hnsw:space": "cosine"},
+)
+
+# now let models run through documents and add to database
+documents = [
+    "The latest iPhone model comes with impressive features and a powerful camera.",
+    "Exploring the beautiful beaches and vibrant culture of Bali is a dream for many travelers.",
+    "Einstein's theory of relativity revolutionized our understanding of space and time.",
+    "Traditional Italian pizza is famous for its thin crust, fresh ingredients, and wood-fired ovens.",
+    "The American Revolution had a profound impact on the birth of the United States as a nation.",
+    "Regular exercise and a balanced diet are essential for maintaining good physical health.",
+    "Leonardo da Vinci's Mona Lisa is considered one of the most iconic paintings in art history.",
+    "Climate change poses a significant threat to the planet's ecosystems and biodiversity.",
+    "Startup companies often face challenges in securing funding and scaling their operations.",
+    "Beethoven's Symphony No. 9 is celebrated for its powerful choral finale, 'Ode to Joy.'",
+]
+
+genres = [
+    "technology",
+    "travel",
+    "science",
+    "food",
+    "history",
+    "fitness",
+    "art",
+    "climate change",
+    "business",
+    "music",
+]
+
+collection.add(
+    documents=documents,
+    ids=[f"id{i}" for i in range(len(documents))],  # [ id0, id1, etc]
+    metadatas=[{"genre": g} for g in genres],
+)
+
+# now search through the database
+query_results = collection.query(query_texts=["Find me some good food"], n_results=2)
+
+query_results
